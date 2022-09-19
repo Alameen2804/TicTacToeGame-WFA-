44932dbb09036f6e575d3fa7665b3d9cc34db1d2
Alameen <94040039+Alameen2804@users.noreply.github.com>
2022-09-19 11:26:11 +0530

    Add files via upload

---
+++ TicTacToeGame/TicTacToeGame/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        TicTacToe Tgame = new TicTacToe();
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag==0)
+            {
+                if (button1.Text=="")
+                {
+                    Tgame.Choice = 0;
+                    Tgame.MarkInBoard();
+                    button1.Text = Convert.ToString(Tgame.Array[0]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text=="")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();   
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag==0)
+            {
+                if (button2.Text=="")
+                {
+                    Tgame.Choice = 1;
+                    Tgame.MarkInBoard();
+                    button2.Text = Convert.ToString(Tgame.Array[1]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button3.Text == "")
+                {
+                    Tgame.Choice = 2;
+                    Tgame.MarkInBoard();
+                    button3.Text = Convert.ToString(Tgame.Array[2]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button4.Text == "")
+                {
+                    Tgame.Choice = 3;
+                    Tgame.MarkInBoard();
+                    button4.Text = Convert.ToString(Tgame.Array[3]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button5.Text == "")
+                {
+                    Tgame.Choice = 4;
+                    Tgame.MarkInBoard();
+                    button5.Text = Convert.ToString(Tgame.Array[4]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button6.Text == "")
+                {
+                    Tgame.Choice = 5;
+                    Tgame.MarkInBoard();
+                    button6.Text = Convert.ToString(Tgame.Array[5]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button7.Text == "")
+                {
+                    Tgame.Choice = 6;
+                    Tgame.MarkInBoard();
+                    button7.Text = Convert.ToString(Tgame.Array[6]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button8.Text == "")
+                {
+                    Tgame.Choice = 7;
+                    Tgame.MarkInBoard();
+                    button8.Text = Convert.ToString(Tgame.Array[7]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (Tgame.Flag == 0)
+            {
+                if (button9.Text == "")
+                {
+                    Tgame.Choice = 8;
+                    Tgame.MarkInBoard();
+                    button9.Text = Convert.ToString(Tgame.Array[8]);
+                }
+                else
+                {
+                    MessageBox.Show("Button is already marked !");
+                }
+            }
+            label3.Text = Tgame.WinOrDraw();
+            if (label3.Text == "")
+            {
+                label2.Text = Tgame.ChanceOfThePlayer();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Tgame = new TicTacToe();
+
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            label2.Text = Tgame.ChanceOfThePlayer();
+            label3.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = Tgame.ChanceOfThePlayer();
+            label3.Text = "";
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        char[] _array = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        public char[] Array
+        {
+            get { return _array; }
+            set { _array = value; }
+        }
+
+        int _player = 1;
+
+        public int Player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        int _choice;
+
+        public int Choice
+        {
+            get { return _choice; }
+            set { _choice = value; }
+        }
+
+        int _flag = 0;
+
+        public int Flag
+        {
+            get { return _flag; }
+            set { _flag = value; }
+        }
+
+        public int CheckWin()
+        {
+            if (Array[0] == Array[1] && Array[1] == Array[2])
+            {
+                return 1;
+            }
+            else if (Array[3] == Array[4] && Array[4] == Array[5])
+            {
+                return 1;
+            }
+            else if (Array[6] == Array[7] && Array[7] == Array[8])
+            {
+                return 1;
+            }
+            else if (Array[0] == Array[3] && Array[3] == Array[6])
+            {
+                return 1;
+            }
+            else if (Array[1] == Array[4] && Array[4] == Array[7])
+            {
+                return 1;
+            }
+            else if (Array[2] == Array[5] && Array[5] == Array[8])
+            {
+                return 1;
+            }
+            else if (Array[0] == Array[4] && Array[4] == Array[8])
+            {
+                return 1;
+            }
+            else if (Array[2] == Array[4] && Array[4] == Array[6])
+            {
+                return 1;
+            }
+            else if (Array[0] != '1' && Array[1] != '2' && Array[2] != '3' && Array[3] != '4' && Array[4] != '5' && Array[5] != '6' && Array[6] != '7' && Array[7] != '8' && Array[8] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public string Board()
+        {
+            return string.Format("     |     |      \n  {0}  |  {1}  |  {2}\n_____|_____|_____ \n     |     |      \n  {3}  |  {4}  |  {5}\n_____|_____|_____\n     |     |      \n  {6}  |  {7}  |  {8}\n     |     |      ", Array[0], Array[1], Array[2], Array[3], Array[4], Array[5], Array[6], Array[7], Array[8]);
+        }
+
+        public string ChanceOfThePlayer()
+        {
+            if (Player % 2 == 0)
+            {
+                return "Player 2 Chance";
+            }
+            else
+            {
+                return "Player 1 Chance";
+            }
+        }
+
+        public void MarkInBoard()
+        {
+            if (Array[Choice] != 'X' && Array[Choice] != 'O')
+            {
+                if (Player % 2 == 0)
+                {
+                    Array[Choice] = 'O';
+                    Player++;
+                }
+                else
+                {
+                    Array[Choice] = 'X';
+                    Player++;
+                }
+            }
+            else
+            {
+                throw new Exception("Sorry the row " + Choice + " is already marked with " + Array[Choice]);
+            }
+            Flag = CheckWin();
+        }
+
+        public string WinOrDraw()
+        {
+            if (Flag == 1)
+            {
+                return "Player " + ((Player % 2) + 1) + " has won";
+            }
+            else if(Flag==-1)
+            {
+                return "Match Draw";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
